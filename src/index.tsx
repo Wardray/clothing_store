2118fff04a3ef59683f74f39dc292543042b95c3
Wardray <v.ladisla.v@mail.ru>
2024-07-18 19:12:55 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,56 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+interface IProductCard {
+  name: string;
+  image: string;
+  price: number;
+}
+interface ICategoryCard {
+  text: string;
+  color: string;
+  icon: React.ReactNode;
+}
+export const ProductCard = (props: IProductCard) => {
+  return (
+    <a>
+      <img src={props.image} alt="Сервис публикации фотографий" />
+    </a>
+  );
+};
+export const CategoryCard = (props: ICategoryCard) => {
+  return (
+    <div
+      style={{
+        borderRadius: 20.61,
+        height: 150,
+        width: 150,
+        background: props.color,
+      }}
+    >
+      <Typography
+        style={{ paddingTop: 20, paddingLeft: 20, paddingRight: 20 }}
+        fontSize={14}
+        strokePanel={StrokePanel.Regular}
+        color={"white"}
+        text={props.text}
+      />
+      <Typography
+        style={{ paddingLeft: 15 }}
+        fontSize={52}
+        strokePanel={StrokePanel.Regular}
+        color={"#FFFFFF1A"}
+        text={"Zelox"}
+      />
+      <div style={{ position: "relative", left: 80, top: -15 }}>
+        {props.icon}
+      </div>
+    </div>
+  );
+};
 root.render(
   <React.StrictMode>
-    <Input initialValue={"Fun10"} />
+    {/* <Input initialValue={"Fun10"} />
     <Select width={100} text="MEDIUM" background="black" textColor={"white"} />
     <Typography
       fontSize={16}
@@ -44,29 +91,96 @@ root.render(
       height={50}
       text={"123"}
     />
-    <div style={{ backgroundColor: "red", width: 640, height: 960 }}>
-      <MainPage
-        leftIcon={
-          <>
-            <Icon
-              backgroundColor="rgba(246, 246, 246, 1)"
-              type={IconType.Circle}
-              icon={"Menu"}
+    <div style={{ backgroundColor: "red", width: 640, height: 960 }}> */}
+    <MainPage
+      leftIcon={
+        <>
+          <Icon
+            backgroundColor="rgba(246, 246, 246, 1)"
+            type={IconType.Circle}
+            icon={"Menu"}
+          />
+        </>
+      }
+      rightIcon={
+        <>
+          <Icon
+            backgroundColor="rgba(246, 246, 246, 1)"
+            type={IconType.Circle}
+            icon={"BagLine"}
+          />
+        </>
+      }
+      header={"123"}
+      children={
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+
+            paddingTop: 50,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "center",
+            }}
+          >
+            <CategoryCard
+              text={"Trending \n Products"}
+              color={"#F4C600"}
+              icon={<Icon icon={"LeftArrow"} />}
             />
-          </>
-        }
-        rightIcon={
-          <>
-            <Icon
-              backgroundColor="rgba(246, 246, 246, 1)"
-              type={IconType.Circle}
-              icon={"BagLine"}
+            <div style={{ width: 15 }}></div>
+            <CategoryCard
+              text={"Sale \n Products"}
+              color={"#EB6434"}
+              icon={<Icon icon={"LeftArrow"} />}
             />
-          </>
-        }
-        header={"123"}
-        children={<div></div>}
-      />
-    </div>
+          </div>
+          <Typography
+            style={{ paddingTop: 30, paddingLeft: 45, paddingRight: 20 }}
+            fontSize={16}
+            strokePanel={StrokePanel.UltraBold}
+            color={"black"}
+            text={"POPULAR PRODUCTS"}
+          />
+          <div style={{ overflow: "scroll", height: "100%" }}>
+            <ProductCard
+              name={"Avoine hooded quilted jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1999}
+            />
+            <ProductCard
+              name={"hooded metallic shell jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1400}
+            />
+            <ProductCard
+              name={"Avoine hooded quilted jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1999}
+            />
+            <ProductCard
+              name={"hooded metallic shell jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1400}
+            />
+            <ProductCard
+              name={"Avoine hooded quilted jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1999}
+            />
+            <ProductCard
+              name={"hooded metallic shell jacket"}
+              image={"https://imageup.ru/img156/thumb/y56nkqfrdus4866356.jpg"}
+              price={1400}
+            />
+          </div>
+        </div>
+      }
+    />
   </React.StrictMode>
 );
